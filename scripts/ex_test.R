9c385991de3ddcc4cc5ddfe2b12b3ff7586e141f
kdorheim <kalyn.dorheim@pnnl.gov>
2024-06-11 15:53:35 -0400

    adding hector to the CI, hopefully

--- scripts/ex_test.R
+++ scripts/ex_test.R
@@ -3,13 +3,15 @@
 source(here::here("scripts", "ex_fxns.R"))
 
 library(assertthat)
-
+library(hector)
 
 # Let's test to see if the right things are returne
 assert_that(is.string(hello_world()))
 assert_that(is.numeric(my_sum(1, 2)))
 
 # Checking to see if the expected value is returned
-assert_that(my_sum(1, 2) == 9)
+assert_that(my_sum(1, 2) == 3)
 
 
+out <- my_hector_run()
+assert_that(is.data.frame(out))
