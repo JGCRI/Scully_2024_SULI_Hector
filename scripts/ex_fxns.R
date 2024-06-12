6fb66d6f78f8a88150f5d787f73bbf3f15a74697
kdorheim <kalyn.dorheim@pnnl.gov>
2024-06-11 20:57:48 -0400

    base working github actions

---
+++ scripts/ex_fxns.R
@@ -0,0 +1,19 @@
+hello_world <- function(){
+  return("hello world")
+}
+
+my_sum <- function(a, b){
+  return(sum(a,b))
+}
+
+# my_hector_run <- function(){
+#
+#   ini <- system.file(package = "hector", "input/hector_ssp119.ini")
+#   core <- newcore(ini)
+#   run(core)
+#   out <- fetchvars(core, dates = 1850:2020)
+#   return(out)
+#
+#
+# }
+
---
+++ scripts/ex_test.R
@@ -0,0 +1,17 @@
+
+# load the functions
+source(here::here("scripts", "ex_fxns.R"))
+
+library(assertthat)
+#library(hector)
+
+# Let's test to see if the right things are returne
+assert_that(is.string(hello_world()))
+assert_that(is.numeric(my_sum(1, 2)))
+
+# Checking to see if the expected value is returned
+assert_that(my_sum(1, 2) == 3)
+
+
+# out <- my_hector_run()
+# assert_that(is.data.frame(out))
