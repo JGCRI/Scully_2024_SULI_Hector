f20b09cee07a5458953801178e903b31fd0b0c22
ptrscll <ptrscll@gmail.com>
2024-06-13 13:11:18 -0400

    Added calc_table_metrics

--- scripts/major_functions.R
+++ scripts/major_functions.R
@@ -6,8 +6,27 @@
 ### Imports and Constants ###
 library(hector)
 library(dplyr)
-
 source(here::here("scripts", "error_functions.R"))
+
+
+# File Paths
+IDEAL_RUNS <- here::here("idealized_inputs")
+INPUT_TCRE <- system.file("input/hector_ssp585.ini", package = "hector")
+INPUT_TCR <- file.path(IDEAL_RUNS, "hector_1pctCO2.ini")
+
+# Ocean heat content constants
+OCEAN_AREA <- 5100656e8 * (1 - 0.29) # The total area of the ocean
+W_TO_ZJ <- 3.155693e-14              # Watts to ZJ
+
+# Constants for TCRE and ERF calculations
+EMISSIONS_VARS <- c(FFI_EMISSIONS(), LUC_EMISSIONS())
+UPTAKE_VARS <- c(LUC_UPTAKE(), DACCS_UPTAKE())
+TCRE_VARS <- c(GLOBAL_TAS(),
+               FFI_EMISSIONS(), LUC_EMISSIONS(),
+               LUC_UPTAKE(), DACCS_UPTAKE())
+
+AEROSOL_RF_VARS <- c(RF_BC(), RF_OC(), RF_NH3(), RF_SO2(), RF_ACI())
+NONGHG_RF_VARS <- c(AEROSOL_RF_VARS, RF_VOL(), RF_ALBEDO(), RF_MISC())
 
 ################################
 ### Comparison Data Fetchers ###
@@ -144,6 +163,49 @@ rel_to_interval <- function(data, var, start, end) {
   return(rel_to_val(data, var, benchmark))
 }
 
+
+# sum_vars - function to sum the values of several Hector variables at each
+#            timestep. adds new rows to the provided data frame containing the
+#            summed values at each time step
+#
+# args:
+#   data - data frame outputted by fetchvars
+#   vars - vector of Hector variables to sum together
+#   name - name to call the new variable containing the sums of the values in
+#           vars
+#   unit - units to assign to the new variable
+#   yrs  - vector of years to sum these variables for
+#
+# returns: original data frame with additional rows containing the sums of the
+#          values for the inputted variables
+#
+# Note: Currently, all columns other than year, variable, and value will be set
+#       equal to the same values as the last row in the original data frame
+# Note: This function is currently inefficient and should ideally be reworked to
+#       take advantage of dplyr functions like group_by
+sum_vars <- function(data, vars, name, unit, yrs) {
+  
+  # Setting up iterator to iterate through new rows of data frame
+  curr_row <- nrow(data) + 1
+  for (yr in yrs) {
+    # Adding new row to data frame
+    data[curr_row,] <- data[curr_row - 1,]
+    data[curr_row,]$year <- yr
+    data[curr_row,]$variable <- name
+    data[curr_row,]$units <- unit
+    
+    # Summing the values across all variables
+    data[curr_row,]$value <-
+      sum(filter(data, year == yr & variable %in% vars)$value)
+    curr_row <- curr_row + 1
+  }
+  return(data)
+}
+
+
+###############################
+### Optim-Related Functions ###
+###############################
 
 # run_hector - function to run Hector using a given ini file and set of params
 #
@@ -297,6 +359,236 @@ run_optim <- function(obj_fn, obs_data, ini_file, params, yrs, vars, error_fn,
   
 }
 
-# Everything below should be copied from the table metrics script
-# TODO: Once finished with that script, copy it in and rework it
-# TODO: Delete old version of run_hector when table making script is copied in
+##############################
+### OTHER OUTPUT FUNCTIONS ###
+##############################
+
+# calc_table_metrics - function to calculate the key values, historical metrics,
+#                      and future predictions for a given set of Hector params
+#
+# args:
+#   params      - vector of Hector params to modify. if NULL, does default run
+#   vals        - vector of values to use for those Hector parameters
+#   output_file - path to file to append table to
+#
+# returns: Nothing, but outputs table to given output file
+calc_table_metrics <- function(params, vals, output_file) {
+
+  ### KEY METRICS ###
+  
+  ## Finding TCRE ##
+  
+  tcre_data <- run_hector(ini_file = INPUT_TCRE,
+                          params = params,
+                          vals = vals,
+                          yrs = 1750:2300,
+                          vars = TCRE_VARS)
+  
+  # Combining emissions data
+  tcre_data %>%
+    subset(variable %in% EMISSIONS_VARS) %>%
+    group_by(year) %>%
+    summarize(all_emissions = sum(value)) %>%
+    .$all_emissions -> tcre_emissions
+  
+  total_emissions <- cumsum(tcre_emissions)
+  
+  tcre_data %>%
+    subset(variable %in% UPTAKE_VARS) %>%
+    group_by(year) %>%
+    summarize(all_uptake = sum(value)) %>%
+    .$all_uptake -> tcre_uptake
+  
+  total_uptake <- cumsum(tcre_uptake)
+  
+  net_emissions <- total_emissions - total_uptake
+  
+  # Getting temperature data
+  tcre_temps <- filter(tcre_data, variable == GLOBAL_TAS())$value
+  
+  # Calculating TCRE via linear regression
+  tcre_reg <- lm(tcre_temps ~ net_emissions)
+  tcre <- tcre_reg$coefficients[2] * 1000
+  
+  
+  ## Finding TCR ##
+  
+  # Running Hector and extracting relevant data
+  tcr_data <- run_hector(ini_file = INPUT_TCR,
+                         params   = params,
+                         vals     = vals,
+                         yrs      = 1800:2000,
+                         vars     = c(GLOBAL_TAS(), CONCENTRATIONS_CO2()))
+  
+  tcr_co2   <- filter(tcr_data, variable == CONCENTRATIONS_CO2())$value
+  tcr_temps <- filter(tcr_data, variable == GLOBAL_TAS())$value
+  
+  # Finding TCR from linear fit
+  tcr_reg <- lm(tcr_temps ~ tcr_co2)
+  
+  # Getting initial CO2
+  core <- newcore(INPUT_TCR)
+  initial_co2 <- fetchvars(core, dates = NA, vars = PREINDUSTRIAL_CO2())$value
+  shutdown(core)
+  
+  tcr <- tcr_reg$coefficients[1] + tcr_reg$coefficients[2] * initial_co2 * 2
+  
+  
+  ### HISTORICAL WARMING AND ERF ###
+  
+  ## Initial Hector Run ##
+  
+  # Setting up variables
+  hist_yrs <- 1750:2019
+  hist_vars <- c(GLOBAL_TAS(),               # For GSAT warming
+                 HEAT_FLUX(),                # For ocean heat content change
+                 RF_CH4(),                   # For Methane ERF
+                 NONGHG_RF_VARS, RF_TOTAL()  # For other ERF calcs
+  )
+  hist_file <- system.file("input/hector_ssp245.ini", package = "hector")
+  
+  # Running Hector
+  hist_data <- run_hector(ini_file = hist_file,
+                          params = params,
+                          vals = vals, 
+                          yrs = hist_yrs, 
+                          vars = hist_vars)
+  
+  
+  ## Finding GSAT Warming ##
+  
+  # Normalizing temperatures
+  hist_data <- rel_to_interval(data  = hist_data,
+                               var   = GLOBAL_TAS(),
+                               start = 1850,
+                               end   = 1900)
+  
+  # Getting 1995-2014 avg
+  GSAT_warming <- get_interval_avg(data  = hist_data,
+                                   var   = GLOBAL_TAS(),
+                                   start = 1995,
+                                   end   = 2014)
+  
+  
+  ## Finding ocean heat content change ##
+  
+  # Summing ocean heat (by taking average and multiplying by number of yrs)
+  avg_flux <- get_interval_avg(data  = hist_data,
+                               var   = HEAT_FLUX(),
+                               start = 1971,
+                               end   = 2018)
+  total_flux <- avg_flux * length(1971:2018)
+  
+  # Converting flux to heat content change
+  ocean_heat_content_change <- total_flux * OCEAN_AREA * W_TO_ZJ
+  
+  
+  ###Finding total aerosol ERF ##
+  
+  # Getting total aerosol forcing for relevant years
+  hist_data <- sum_vars(data = hist_data,
+                        vars = AEROSOL_RF_VARS,
+                        name = "tot_aer_ERF",
+                        unit = "w/m^2",
+                        yrs  = 2005:2015)
+  
+  # Getting average forcing from 2005-2015
+  tot_aer_erf <- get_interval_avg(data  = hist_data,
+                                  var   = "tot_aer_ERF",
+                                  start = 2005,
+                                  end   = 2015)
+  
+  ## Finding WMGHG ERF ##
+  
+  # Getting total non-GHG ERF
+  hist_data <- sum_vars(data = hist_data,
+                        vars = NONGHG_RF_VARS,
+                        name = "non_ghg_ERF",
+                        unit = "W/m^2",
+                        yrs  = 2019)
+  
+  # Subtracting non-GHG ERF from total RF
+  wmghg_erf <- filter(hist_data, variable == RF_TOTAL() & year == 2019)$value -
+    filter(hist_data, variable == "non_ghg_ERF" & year == 2019)$value
+  
+  
+  ## Finding methane ERF ##
+  methane_erf <- filter(hist_data, variable == RF_CH4() & year == 2019)$value
+  
+  
+  ### FUTURE WARMING ###
+  
+  # Setting up variables
+  future_yrs <- 1995:2100
+  future_vars <- GLOBAL_TAS()
+  
+  # Getting the names of each scenario file
+  scenarios <- c("119", "126", "245", "370", "585")
+  scenario_names <- paste("input/hector_ssp", scenarios, ".ini", sep = "")
+  scenario_files <- system.file(scenario_names, package = "hector")
+  
+  # Setting up results data frame
+  future_results <- data.frame(matrix(nrow = 0, ncol = 4))
+  colnames(future_results) <- c("scenario", "start", "end", "GSAT")
+  
+  for (scen_counter in 1:length(scenario_files)) {
+    # Getting data
+    scen_data <- run_hector(ini_file = scenario_files[scen_counter],
+                            params = params,
+                            vals = vals,
+                            yrs = future_yrs,
+                            vars = future_vars)
+    
+    # Normalizing temperatures
+    scen_data <- rel_to_interval(data = scen_data,
+                                 var = GLOBAL_TAS(),
+                                 start = 1995,
+                                 end = 2014)
+    
+    # Finding the averages for each time interval
+    start_yrs <- c(2021, 2041, 2081)
+    end_yrs <- start_yrs + 19
+    
+    # Getting the necessary information for each time interval
+    for (i in 1:length(start_yrs)) {
+      
+      # Finding average temperatures for the given time interval
+      temps <- get_interval_avg(data = scen_data,
+                                var = GLOBAL_TAS(),
+                                start = start_yrs[i],
+                                end = end_yrs[i])
+      
+      # Adding the scenario, start and end dates, and avg temperature to the
+      # data frame
+      future_results[nrow(future_results) + 1, ] <-
+        list(paste("ssp", scenarios[scen_counter], sep = ""),
+             start_yrs[i],
+             end_yrs[i],
+             signif(temps, 3))
+    }
+  }
+  
+  
+  ### OUTPUTTING RESULTS ###
+  write("***Key Metrics***", file = output_file)
+  write_metric("TCRE:", tcre, output_file)
+  write_metric("TCR: ", tcr, output_file)
+  write("", file = output_file, append = TRUE)
+  
+  write("***Historical Warming and ERF***", file = output_file, append = TRUE)
+  write_metric("GSAT Warming:             ", GSAT_warming, output_file)
+  write_metric("Ocean Heat Content Change:", ocean_heat_content_change, 
+               output_file)
+  write_metric("Total Aerosol ERF:        ", tot_aer_erf, output_file)
+  write_metric("WMGHG ERF:                ", wmghg_erf, output_file)
+  write_metric("Methane ERF:              ", methane_erf, output_file)
+  write("", file = output_file, append = TRUE)
+  
+  write("***Future Warming***", file = output_file, append = TRUE)
+  write.table(future_results,
+              file = output_file,
+              append = TRUE,
+              quote = FALSE,
+              sep = "\t",
+              row.names = FALSE)
+}
--- scripts/scratch.R
+++ scripts/scratch.R
@@ -16,40 +16,41 @@ MAJ_FUNC_PATH <- file.path(SCRIPTS_DIR, "major_functions.R")
 # Importing functions
 source(MAJ_FUNC_PATH)
 
-# Testing get_var_mse
-data1 <- get_co2_data(CO2_PATH)
+# # Testing get_var_mse
+# data1 <- get_co2_data(CO2_PATH)
+# 
+# ini_file <- system.file("input/hector_ssp245.ini", package = "hector")
+# data2 <- run_hector(ini_file, 
+#                     params = NULL, 
+#                     vals = NULL, 
+#                     yrs = 1959:2023, 
+#                     vars = c(CONCENTRATIONS_CO2()))
+# 
+# get_var_mse(data1, data2, CONCENTRATIONS_CO2(), 1959, 2023)
+# 
+# 
+# # Testing objective_fn
+# 
+# # Creating an error function
+# get_co2_mse <- function(obs_data, hector_data) {
+#   return(get_var_mse(obs_data = obs_data, 
+#                      hector_data = hector_data, 
+#                      var = CONCENTRATIONS_CO2(),
+#                      start = 1959,
+#                      end = 2023))
+# }
+# 
+# # This call should match get_var_mse
+# objective_fn(obs_data = data1, 
+#              ini_file = system.file("input/hector_ssp245.ini", package = "hector"),
+#              params = NULL, 
+#              par = NULL, 
+#              yrs = 1959:2023, 
+#              vars = c(CONCENTRATIONS_CO2()), 
+#              error_fn = get_co2_mse)
 
-ini_file <- system.file("input/hector_ssp245.ini", package = "hector")
-data2 <- run_hector(ini_file, 
-                    params = NULL, 
-                    vals = NULL, 
-                    yrs = 1959:2023, 
-                    vars = c(CONCENTRATIONS_CO2()))
-
-get_var_mse(data1, data2, CONCENTRATIONS_CO2(), 1959, 2023)
-
-
-# Testing objective_fn
-
-# Creating an error function
-get_co2_mse <- function(obs_data, hector_data) {
-  return(get_var_mse(obs_data = obs_data, 
-                     hector_data = hector_data, 
-                     var = CONCENTRATIONS_CO2(),
-                     start = 1959,
-                     end = 2023))
-}
-
-# This call should match get_var_mse
-objective_fn(obs_data = data1, 
-             ini_file = system.file("input/hector_ssp245.ini", package = "hector"),
-             params = NULL, 
-             par = NULL, 
-             yrs = 1959:2023, 
-             vars = c(CONCENTRATIONS_CO2()), 
-             error_fn = get_co2_mse)
-
-#data3 <- get_temp_data(TEMP_PATH)
-
-#rbind(data1, data2, data3)
+# Testing calc_table_metrics
+calc_table_metrics(params = NULL,
+                   vals = NULL,
+                   output_file = here::here("results", "original_params.txt"))
 
