78689fe2d5213e1324ce2b62e8b84279c3c97bd4
ptrscll <ptrscll@gmail.com>
2024-06-25 11:34:41 -0400

    Added basic unit tests for mvsse

--- tests/error_tests.R
+++ tests/error_tests.R
@@ -87,9 +87,21 @@ nmse_unc_tests <- function() {
                        c(10, 7,  28)) == 9 / 625)
 }
 
+# Trying to use mvsse
+mvsse_tests <- function () {
+  # Just using mse tests and sd of 1
+  assert_that(mvsse(0, 1, 0) == 0)
+  assert_that(mvsse(c(0, 1, 2), 1, c(1, 2, 1)) == 1)
+  assert_that(mvsse(c(0, 1, 2), c(1, 1, 1), c(0, 1, -1)) == 3)
+  assert_that(mvsse(c(-2, 1, 2), 1, c(0, 3, 0)) == 4)
+  
+  # Other tests
+  assert_that(mvsse(c(0, 1, 2, 3), c(2, 3, 4, 1), c(-1, 4, 4, 3)) == 0.375)
+}
 
 # Calling all testing functions
 mse_tests()
 get_var_mse_tests()
 mse_unc_tests()
 nmse_unc_tests()
+mvsse_tests()
